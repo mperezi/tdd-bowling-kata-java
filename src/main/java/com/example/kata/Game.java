60994ff40273ae52ea49b65f9cb6765e27518e36
mperezi <mperezibars@gmail.com>
2022-02-27 08:45:46 +0100

    Refactor

--- src/main/java/com/example/kata/Game.java
+++ src/main/java/com/example/kata/Game.java
@@ -16,27 +16,37 @@ public class Game {
     int currentFramePinsDown;
 
     public void roll(int pinsDown) {
-        if (lastFramePinsDown == 10 &&  lastFrameTries == 2 && currentTry == 1) {
+        if (lastFrameWasSpare() && currentTry == 1) {
             score += pinsDown;
         }
 
         score += pinsDown;
         currentFramePinsDown += pinsDown;
 
+        advanceFrame(pinsDown);
+    }
+
+    private boolean lastFrameWasSpare() {
+        return lastFramePinsDown == 10 && lastFrameTries == 2;
+    }
+
+    private void advanceFrame(int pinsDown) {
         if (currentTry == 1) {
             if (pinsDown < 10) {
                 currentTry = 2;
             } else {  // strike
-                lastFramePinsDown = currentFramePinsDown;
-                currentFramePinsDown = 0;
-                lastFrameTries = 1;
+                resetFrame(1);
             }
         } else {
-            currentTry = 1;
-            lastFramePinsDown = currentFramePinsDown;
-            currentFramePinsDown = 0;
-            lastFrameTries = 2;
+            resetFrame(2);
         }
+    }
+
+    private void resetFrame(int lastFrameTries) {
+        currentTry = 1;
+        lastFramePinsDown = currentFramePinsDown;
+        currentFramePinsDown = 0;
+        this.lastFrameTries = lastFrameTries;
     }
 
     public int score() {
